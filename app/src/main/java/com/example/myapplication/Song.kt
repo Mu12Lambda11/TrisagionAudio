b0821c5c7891b79c67c5075c87c6592147488134
Mu12Lambda11 <idjordan@cpp.edu>
2023-11-24 17:59:06 -0800

    xml files initialized

--- app/src/main/java/com/example/myapplication/Song.kt
+++ app/src/main/java/com/example/myapplication/Song.kt
@@ -1,4 +1,12 @@
 package com.example.myapplication
-
-class Song {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+import java.util.UUID
+@Entity
+data class Song (
+    @PrimaryKey val title : String,
+        val length : String,
+        val artist: String,
+        val album: String,
+)
