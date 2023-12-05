a0792dd97354200e63f4d0595d21679b9d4e6364
Mu12Lambda11 <idjordan@cpp.edu>
2023-12-04 23:30:09 -0800

    Mixer done

--- app/src/main/java/com/example/myapplication/MixerFragment.kt
+++ app/src/main/java/com/example/myapplication/MixerFragment.kt
@@ -11,8 +11,8 @@ import android.view.ViewGroup
 import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
 import com.example.myapplication.databinding.FragmentMixerBinding
+import kotlin.properties.Delegates
 
 
 class MixerFragment : Fragment() {
@@ -24,11 +24,23 @@ class MixerFragment : Fragment() {
     private val mixerViewModel: MixerViewModel by viewModels()
     lateinit var mediaPlayer: MediaPlayer
     lateinit var eq: Equalizer
+    var lowerBandEqRange by Delegates.notNull<Short>()
+    var upperBandEqRange by Delegates.notNull<Short>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mediaPlayer=mixerViewModel.mediaPlayer
-        eq= Equalizer(1,mediaPlayer.audioSessionId)
+
+        //fill out the eq
+        eq= Equalizer(0,mediaPlayer.audioSessionId)
+        eq.enabled=false
+        lowerBandEqRange= eq.bandLevelRange[0]
+        upperBandEqRange= eq.bandLevelRange[1]
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 
     override fun onCreateView(
@@ -37,70 +49,10 @@ class MixerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding=FragmentMixerBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        //Runnable object to parse the current song position
-        this.activity?.runOnUiThread(object: Runnable{
-            override fun run() {
-                //low band bar
-                binding.lowBandBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-                    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-
-                    }
-
-                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-                    }
-
-                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-                    }
-                })
-                //mid band bar
-                binding.midBandBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-                    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-
-                    }
-
-                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-                    }
-
-                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-                    }
-                })
-                //high band bar
-                binding.highBandBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-                    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-
-                    }
-
-                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-                    }
-
-                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-                    }
-                })
-
-                fillSeekbar(this)
-
-            }
-
-        })
-
-
-    }
-    fun fillSeekbar(runner :Runnable){
         //Fill Out seekbar
-        val currentPosition = 50
-        val seekbarDuration = 100
+        val seekbarDuration = upperBandEqRange-lowerBandEqRange
+        val currentPosition = seekbarDuration/2
 
         binding.lowBandBar.progress=currentPosition
         binding.midBandBar.progress=currentPosition
@@ -109,6 +61,94 @@ class MixerFragment : Fragment() {
         binding.lowBandBar.max=seekbarDuration
         binding.midBandBar.max=seekbarDuration
         binding.highBandBar.max=seekbarDuration
-        val handler = Handler(Looper.getMainLooper()).postDelayed(runner, 1000)
+
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        eq.setBandLevel(1, binding.lowBandBar.progress.toShort())
+        eq.setBandLevel(2, binding.midBandBar.progress.toShort())
+        eq.setBandLevel(3, binding.highBandBar.progress.toShort())
+
+        //Runnable object to parse the current song position
+        requireActivity().runOnUiThread(object: Runnable{
+            override fun run() {
+                runSeekbars()
+
+            }
+
+        })
+
+        binding.apply {
+            eqButton.setOnClickListener {
+                if(!eq.enabled){
+                    eq.enabled=true
+                    eqButton.setImageResource(R.drawable.ic_equalizer_on)
+                    eqBtnText.text="ON"
+                }else{
+                    eq.enabled=false
+                    eqButton.setImageResource(R.drawable.ic_equalizer_off)
+                    eqBtnText.text="OFF"
+                }
+            }
+            backButton.setOnClickListener {
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+        }
+
+    }
+
+    private fun runSeekbars() {
+        //low band bar
+        binding.lowBandBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                eq.setBandLevel(1, (progress+lowerBandEqRange).toShort())
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+        //mid band bar
+        binding.midBandBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                eq.setBandLevel(2, (progress+lowerBandEqRange).toShort())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+        //high band bar
+        binding.highBandBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                eq.setBandLevel(3, (progress+lowerBandEqRange).toShort())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+    }
+
+
+
+
 }
